bd4e046c053fe0ffa8e8afa2dd1b066c88c04da9
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-08-14 02:00:10 -0400

    realtime playback with voice

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import sound.SoundRecorder;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
-//        final SoundRecorder recorder = new SoundRecorder();
-//
+        final SoundRecorder recorder = new SoundRecorder();
+
 //        Thread stopper = new Thread(new Runnable() {
 //            @Override
 //            public void run() {
 //                try{
-//                    Thread.sleep(60000);
+//                    Thread.sleep(20000);
 //                }catch(InterruptedException ex){
 //                    ex.printStackTrace();
 //                }
@@ -18,8 +18,8 @@ public class Main {
 //            }
 //        });
 //        stopper.start();
-//
-//        recorder.start();
+
+        recorder.start();
 //        try {
 //            new SoundPlayer();
 //        }catch(Exception ex){
--- src/sound/SoundRecorder.java
+++ src/sound/SoundRecorder.java
@@ -1,61 +1,48 @@
 package sound;
 
 import javax.sound.sampled.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
 public class SoundRecorder {
-    TargetDataLine line;
-
-    File wavFile = new File("C:\\Users\\Jake Paul\\Desktop\\Programming\\Java\\udp-voip\\samples\\RecordAudio.wav");
-
-    // format of audio file
-    AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
-
-    public AudioFormat getAudioFormat() {
-        float sampleRate = 16000;
-        int sampleSizeInBits = 8;
-        int channels = 2;
-        boolean signed = true;
-        boolean bigEndian = true;
-        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,
-                channels, signed, bigEndian);
-        return format;
-    }
-
     public void start() {
+        AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
+        TargetDataLine microphone;
+        SourceDataLine speakers;
         try {
-            AudioFormat format = getAudioFormat();
             DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            microphone = (TargetDataLine) AudioSystem.getLine(info);
+            microphone.open(format);
 
-            // checks if system supports the data line
-            if (!AudioSystem.isLineSupported(info)) {
-                System.out.println("Line not supported");
-                System.exit(0);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            int numBytesRead;
+            int CHUNK_SIZE = 1024;
+//            byte[] data = new byte[microphone.getBufferSize() / 5];
+            microphone.start();
+
+            int bytesRead = 0;
+            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
+            speakers = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+            speakers.open(format);
+            speakers.start();
+
+            while (bytesRead < 300000) {
+                byte[] newData = new byte[CHUNK_SIZE];
+                numBytesRead = microphone.read(newData, 0, CHUNK_SIZE);
+                bytesRead += numBytesRead;
+                // write the mic data to a stream for use later
+                out.write(newData, 0, numBytesRead);
+                // write mic data to stream for immediate playback
+                speakers.write(newData, 0, numBytesRead);
             }
-            line = (TargetDataLine) AudioSystem.getLine(info);
-            line.open(format);
-            line.start();   // start capturing
 
-            System.out.println("Start capturing...");
-
-            AudioInputStream ais = new AudioInputStream(line);
-
-            System.out.println("Start recording...");
-
-            // start recording
-            AudioSystem.write(ais, fileType, wavFile);
-
-        } catch (LineUnavailableException ex) {
-            ex.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            speakers.drain();
+            speakers.close();
+            microphone.close();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
         }
-    }
-
-    public void finish() {
-        line.stop();
-        line.close();
-        System.out.println("Finished");
     }
 }
